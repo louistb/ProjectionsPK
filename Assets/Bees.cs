d4b8f159b5da86b0d69c1058c82149272870443c
Louis t.b <louistb@me.com>
2017-12-02 13:16:34 -0500

    Unity Project

---
+++ Assets/Bees.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bees : MonoBehaviour {
+
+	public float _MaxRefreshRate,_TimeBeforeDeath,_speed;
+	public float _RangeDirectionY,_RangeDirectionX,_RangeDirectionZ;
+	public BeesController Controller;
+
+	public float LifeTimer,t,UpdateTime;
+	public Vector3 CurrentPos, DestinationPos,AtractPos;
+	public bool InAttraction;
+
+	void Awake() {
+		Controller = GameObject.Find ("Bees_System").GetComponent<BeesController>();
+
+		_MaxRefreshRate = Controller.MaxRefreshRate;
+		_TimeBeforeDeath = Controller.TimeBeforeDeath - Controller.TimeBeforeDeath / Random.Range(0,Controller.TimeBeforeDeath/3);
+		_speed = Controller.speed;
+		_RangeDirectionX = Controller.RangeDirectionX;
+		_RangeDirectionY = Controller.RangeDirectionY;
+		_RangeDirectionZ = Controller.RangeDirectionZ;
+	}
+
+	void Start () 
+	{
+		UpdateTime = NewRandUpdate();
+		DirectionUpdate();
+	}
+		
+
+	void OnCollisionEnter(Collision collision)
+	{
+		print (collision);
+		DirectionUpdate ();
+	}
+
+	private void DirectionUpdate() {
+		UpdateTime = NewRandUpdate();
+		t = 0;	
+		if (InAttraction = true) {
+			DestinationPos.x = AtractPos.x + Random.Range (-_RangeDirectionX / 10, _RangeDirectionX / 10);
+			DestinationPos.y = AtractPos.y + Random.Range (-_RangeDirectionY / 10, _RangeDirectionY / 10);
+			DestinationPos.z = AtractPos.z + Random.Range (0,-_RangeDirectionZ / 10);
+		} else {
+			DestinationPos.x = transform.position.x + Random.Range (-_RangeDirectionX, _RangeDirectionX);
+			DestinationPos.y = transform.position.y + Random.Range (-_RangeDirectionY, _RangeDirectionY);
+			DestinationPos.z = transform.position.z + Random.Range (0,-_RangeDirectionZ);
+		}
+	}
+
+	private void FixedUpdate () 
+	{
+		//GLOBAL 
+		t += Time.deltaTime;
+
+		if (t >= UpdateTime) {
+			DirectionUpdate();
+		}
+
+		//LERP
+		CurrentPos = transform.position;
+
+		transform.position = Vector3.Slerp(CurrentPos,DestinationPos, t * _speed);
+
+		//KILLING WHEN TIME IS OVER
+		LifeTimer += Time.deltaTime;
+
+		if (LifeTimer >= _TimeBeforeDeath) {
+			Destroy (gameObject);
+		}
+	}
+
+	public float NewRandUpdate() {
+		return Random.Range(0, _MaxRefreshRate);
+	}
+		
+}
---
+++ Assets/BeesController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeesController : MonoBehaviour {
+
+	public GameObject bees;
+	public int NbOfBees;
+	public float MaxRefreshRate,TimeBeforeDeath,speed;
+	public float RangeDirectionY,RangeDirectionX,RangeDirectionZ;
+
+
+	private GameObject[] intheScene;
+
+	void Update () {
+		
+		if(Input.GetKeyDown(KeyCode.Space)) {
+			NewBees();
+		}
+
+		if (Input.GetKeyDown (KeyCode.C)) {
+			intheScene = GameObject.FindGameObjectsWithTag ("bee");
+			print (intheScene.Length);
+		}
+
+		if (Input.GetKeyDown (KeyCode.D)) {
+			intheScene = GameObject.FindGameObjectsWithTag ("bee");
+			foreach (var bees in intheScene) {
+				Destroy (bees);
+			}
+		}
+		if(Input.GetMouseButtonDown(0)) {
+			intheScene = GameObject.FindGameObjectsWithTag ("bee");
+			foreach (var bees in intheScene) {
+				var beesScript = bees.GetComponent<Bees>();
+				beesScript.InAttraction = true;
+			}
+		}
+
+		if(Input.GetMouseButton(0)) {
+			foreach (var bees in intheScene) {
+				if (bees != null) {
+				var beesScript = bees.GetComponent<Bees>();
+				var mouseRay = Camera.main.ScreenPointToRay (Input.mousePosition).direction;
+				beesScript.AtractPos = new Vector3 (mouseRay.x, mouseRay.y, 0f);
+				}
+			}
+		}
+
+		if(Input.GetMouseButtonUp(0)) {
+			foreach (var bees in intheScene) {
+				if (bees != null) {
+				var beesScript = bees.GetComponent<Bees>();
+				beesScript.InAttraction = false;
+				}
+			}
+		}
+
+	}
+	void NewBees() {
+		for (var i = 0; i <= NbOfBees; i++) {
+			var newRotation =  Quaternion.Euler(-90f, 0f, 0f);
+			GameObject NewBee = Instantiate(bees, transform.position,newRotation,transform) as GameObject;
+			NewBee.tag = "bee";
+		}
+	}
+			
+}
