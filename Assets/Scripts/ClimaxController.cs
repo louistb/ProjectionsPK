da4d31f7ac367b65bfbb80622ae89abc20975680
charlidq <charlidq@gmail.com>
2018-01-15 20:18:08 -0500

    New Controller

---
+++ Assets/Scripts/ClimaxController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClimaxController : MonoBehaviour {
+
+    public GameObject[] Climax;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            var ClimaxObj = Instantiate(Climax[Random.Range(0, Climax.Length)], transform.position, transform.rotation) as GameObject;
+        }
+	}
+}
---
+++ Assets/Scripts/NoisePiston.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoisePiston : MonoBehaviour
+{
+    Vector3 startPos;
+
+    private float amplitude = 0.3f;
+    private float period;
+
+    protected void Start()
+    {
+        startPos = transform.position;
+        period = Random.Range(1f,3f);
+    }
+
+    protected void Update()
+    {
+        float theta = Time.timeSinceLevelLoad / period;
+        float distance = amplitude * Mathf.Sin(theta);
+        transform.position = startPos + Vector3.forward * distance;
+    }
+}
